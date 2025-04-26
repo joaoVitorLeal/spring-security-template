f523928b8e851343a3c88258c0582f37ba40a221
joaoVitorLeal <joaoleal98@outlook.com>
2025-04-25 23:23:43 -0300

    initial commit

---
+++ src/test/java/io/github/joaoVitorLeal/spring_security_template/SpringSecurityTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaoVitorLeal.spring_security_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
